e9c4eb3ed3af2004fd4705124e4cb62dc9f263cc
Delton Mukoya Kuya <deltonmukoyakuya@gmail.com>
2026-01-29 15:31:16 +0300

    fix: run migrations in start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,6 +3,10 @@
 # Substitute the environment variables in the nginx config
 envsubst '$PORT $GS_BUCKET_NAME' < /app/nginx.conf.template > /app/nginx.conf
 
+# Run migrations
+echo "Running migrations..."
+python manage.py migrate --noinput
+
 # Start Gunicorn in the background on port 8000
 echo "Starting Gunicorn on port 8000..."
 gunicorn --bind 127.0.0.1:8000 --workers 1 --threads 8 --timeout 0 church_app.wsgi:application &
